c64d78e22d9c924beec8db4dc1f738f482358f8e
Miranda-Stuart <mirandastuart@mmc.school.nz>
2025-05-09 14:27:52 +1200

    Working yes / no function

--- C_01_yes_no_basic.py
+++ C_01_yes_no_basic.py
@@ -1,8 +1,24 @@
-want_instructions = input("Do you want to see the instructions? ").lower()
-# check the user says yes or no
-if want_instructions == "yes" or want_instructions == "y":
-    print("You said yes!")
-elif want_instructions == "no" or want_instructions == "n":
-    print("You said no.")
-else:
-    print("Please enter yes or no")
+# Functions go here
+
+def yes_no(question):
+
+     # Checks user response to a question is yes/no (y/n); returns "yes" or "no"
+
+     while True:
+
+          response = input(question).lower()
+
+          # check the user says yes or no
+          if response == "yes" or response == "y":
+               return "yes"
+          elif response == "no" or response == "n":
+               return "no"
+          else:
+               print("Please enter yes / no")
+
+# Main Routine
+
+want_instructions = yes_no("Do you want to see the instructions? ").lower()
+want_coffee = yes_no("Do you want coffee? ").lower()
+
+print("We done")
